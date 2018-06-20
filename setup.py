d5b7a0fb42bf370273ad6b396658f4443a68a1be
Devin Bjelland <devin.bjelland@centriam.com>
2018-06-20 14:54:11 -0500

    upgrade cryptography+drop support for python2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='credstash',
-    version='1.14.0',
+    version='1.14.1',
     description='A utility for managing secrets in the cloud using AWS KMS and DynamoDB',
     license='Apache2',
     url='https://github.com/LuminalOSS/credstash',
@@ -14,7 +14,7 @@ setup(
     scripts=['credstash.py'],
     py_modules=['credstash'],
     install_requires=[
-        'cryptography>=1.5, <2.1',
+        'cryptography>=1.5, <=2.3',
         'boto3>=1.1.1',
     ],
     extras_require={
